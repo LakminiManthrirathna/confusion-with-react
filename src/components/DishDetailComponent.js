7b11a88bca8517e877fb5902ed22f092ffb0fedc
lakminim <manthrirathna.mal@gmail.com>
2020-12-03 23:52:28 +0530

    week 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {
     Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle
+    CardTitle,Breadcrumb, BreadcrumbItem
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderComments({comments}) {
     if (comments != null) {
@@ -36,7 +37,6 @@ function RenderDish({dish}) {
                 </div>
                 <div className="col-md-5">
                     <h4>Comments</h4>
-                    <RenderComments comments={dish.comments}/>
                 </div>
             </div>
         );
@@ -48,7 +48,27 @@ function RenderDish({dish}) {
 
 const DishDetail = (props) => {
     return (
-        <RenderDish dish={props.dish}/>
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+        </div>
     );
 }
 
