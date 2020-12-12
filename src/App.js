f879cda5e8cc16b8618d42f19e1dff02d1048ec7
lakminim <manthrirathna.mal@gmail.com>
2020-12-12 13:01:45 +0530

    forms

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import Main from './components/MainComponent';
 import {DISHES} from './shared/dishes';
 import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {configure} from './redux/configure'
+
+const store = configure();
 
 function App() {
     const [dishes, setDishes] = useState(DISHES);
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Main/>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
